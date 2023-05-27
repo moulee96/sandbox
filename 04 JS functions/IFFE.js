4ac244d6bb587cd7a2b281cf0ea38f54003a1b81
moulee96 <63225402+moulee96@users.noreply.github.com>
2023-05-27 20:11:35 +0530

    functions

---
+++ 04 JS functions/IFFE.js
@@ -0,0 +1,14 @@
+const num1=20;
+const num2=30;
+
+function add(){
+console.log(`${num1 + num2}`);
+}
+
+add();
+
+(function(){
+const num1 = 1;
+const num2 = 6;
+console.log(`${num1 + num2}`)
+})()
